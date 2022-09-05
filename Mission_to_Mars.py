2acff8c5432a807ffd15152e2639357f7a92293c
melpatjen16 <mp.jenkins16@gmail.com>
2022-09-05 19:13:34 -0400

    Update for Module

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -1,22 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd 
+import pandas as pd
 
+
+# Set up Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
+
 
 # Visit the mars nasa news site
 url = 'https://redplanetscience.com'
 browser.visit(url)
+
+
 # Optional delay for loading the page
 browser.is_element_present_by_css('div.list_text', wait_time=1)
 
+
+# Convert the browser html to a soup object and then quit the browser
 html = browser.html
 news_soup = soup(html, 'html.parser')
 slide_elem = news_soup.select_one('div.list_text'
@@ -33,7 +37,7 @@ news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 news_p
 
 
-# ### Featured Images
+# ### JPL Space Images Featured Image
 
 # Visit URL
 url = 'https://spaceimages-mars.com'
@@ -55,8 +59,12 @@ img_url_rel
 # Use the base URL to create an absolute URL
 img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 img_url
+                                  
 
+# ## Mars Facts
 df = pd.read_html('https://galaxyfacts-mars.com')[0]
+df.head()
+                                  
 df.columns=['Description', 'Mars', 'Earth']
 df.set_index('Description', inplace=True)
 df
